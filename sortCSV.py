463fb1f57010b83ce38ce11846a31b44efd593fb
Laura Maldonado <138335041+lauraflm@users.noreply.github.com>
2025-06-30 01:53:44 -0400

    add new sortCSV.py
    Considera ordenar por Id y luego por fecha. 
    El cambio recae en que se considera la expresión regular.

--- sortCSV.py
+++ sortCSV.py
@@ -1,12 +1,7 @@
 import pandas as pd
-
-
-df = pd.read_csv("Books_rating.csv")
+df = pd.read_csv("Books_rating.csv", sep=r',(?=(?:[^"]*"[^"]*")*[^"]*$)', engine='python')
 
 df["review/time"] = pd.to_numeric(df["review/time"], errors="coerce")
 
-# hacer el sort 
-df = df.sort_values(by="review/time")
-
-# guardar
-df.to_csv("sorted.csv", index=False)
+df = df.sort_values(by=["Id", "review/time"])
+df.to_csv("sorted2.csv", index=False)
